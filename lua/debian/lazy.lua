b64f62ecce5e9ce1b0b272726bd33bb4afb9fd63
EmmanuelDodoo <custosdodoo@gmail.com>
2023-09-16 15:21:34 -0400

    Added Telescope preview fix for nvim 0.10

--- lua/debian/lazy.lua
+++ lua/debian/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 
 	-- Telescope
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		'nvim-telescope/telescope.nvim', branch = '0.1.x',
 		-- or                              , branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
