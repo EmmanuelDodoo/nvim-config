0c8ca7708d77a3e61650bd8be5df44f194f7b607
EmmanuelDodoo <custosdodoo@gmail.com>
2023-09-19 15:39:39 -0400

    Added keymap for live grep and formatted lazy.lua

--- lua/debian/lazy.lua
+++ lua/debian/lazy.lua
@@ -1,77 +1,80 @@
 ---@diagnostic disable: undefined-global
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	"folke/neodev.nvim",
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim",         cmd = "Neoconf" },
+    "folke/neodev.nvim",
 
-	-- Telescope
-	{
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
+    -- Telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
-	-- Tree sitter
-	{
-		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
-	},
+    -- Tree sitter
+    {
+        "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    },
 
 
-	-- Undo tree
-	{'mbbill/undotree'},
+    -- Undo tree
+    { 'mbbill/undotree' },
 
-	-- Telescope icons
-	{'nvim-tree/nvim-web-devicons'},
+    -- Telescope icons
+    { 'nvim-tree/nvim-web-devicons' },
 
-	-- Theme
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+    -- Theme
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
 
-	-- Git related plugins
-	{'tpope/vim-fugitive'},
-	{'tpope/vim-rhubarb'},
+    -- Git related plugins
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-rhubarb' },
 
-	-- Harpoon
-	{'ThePrimeagen/harpoon'},
+    -- Harpoon
+    { 'ThePrimeagen/harpoon' },
 
-	-- LSP Zero
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- LSP Zero
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {
+                'neovim/nvim-lspconfig',
+            },                                       -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    },
 
     -- Indentation guides
     {
         -- Add indentation guides even on blank Lines
-        "lukas-reineke/indent-blankline.nvim" ,
+        "lukas-reineke/indent-blankline.nvim",
         -- Enable 'Lukas-reineke/indent-bLanRLine. nvim
         -- See :help indent_bLankLine. txt'
 
