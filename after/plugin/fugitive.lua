33f344159e5d74cce0858f19f27a85e7b8ebedd3
EmmanuelDodoo <custosdodoo@gmail.com>
2023-09-27 01:01:53 -0400

    Actually added the git keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
 vim.keymap.set("n", "<leader>gac", function()
     local buf_path = vim.fn.expand("%")
     vim.cmd("Git add " .. buf_path)
@@ -11,3 +12,11 @@ end, { desc = "[g]it [a]dd [a]ll " })
 vim.keymap.set("n", "<leader>gc", function()
     vim.cmd("Git commit")
 end, { desc = "[g]it commit" })
+
+vim.keymap.set("n", "leader>gps", function()
+    vim.cmd("Git push")
+end, { desc = "[g]it [p]u[s]h" })
+
+vim.keymap.set("n", "<leader>gpl", function()
+    vim.cmd("Git pull")
+end, { desc = "[g]it [p]ul[l]" })
