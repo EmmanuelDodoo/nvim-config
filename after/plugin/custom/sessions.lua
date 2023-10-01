d738568d78368940dcf1cfc59ce4631c7923d01e
EmmanuelDodoo <custosdodoo@gmail.com>
2023-09-30 22:28:02 -0400

    Changed location of nvim config files to .nvim dir

--- after/plugin/custom/sessions.lua
+++ after/plugin/custom/sessions.lua
@@ -1,5 +1,5 @@
 function Sessions(file)
-    local file = vim.fn.getcwd() .. "/" .. file
+    local file = vim.fn.getcwd() .. "/.nvim/" .. file
 
     if vim.fn.filereadable(file) == 1 then
         vim.cmd("source " .. file)
