bda82ff7cb923af24ed26c78c478651329feb2be
EmmanuelDodoo <custosdodoo@gmail.com>
2023-10-19 23:01:51 -0400

    [T#9] Uncomment deletes last character in line

---
+++ after/plugin/zz-custom/sessions.lua
@@ -0,0 +1,9 @@
+function Sessions(file)
+    local file = vim.fn.getcwd() .. "/.nvim/" .. file
+
+    if vim.fn.filereadable(file) == 1 then
+        vim.cmd("source " .. file)
+    end
+
+    vim.cmd("autocmd VimLeavePre * mksession! " .. file)
+end
