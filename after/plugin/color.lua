f34329b264b72a6f8c29e8551ef78dbe8abbdd4b
EmmanuelDodoo <custosdodoo@gmail.com>
2023-10-28 15:12:30 -0400

    Background Changes
    - Removed transparent background
    - Changed default theme to nightfly

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,9 +1,10 @@
 function ColorMePlease(color)
-    color = color or "tokyonight-moon"
+--     color = color or "tokyonight-moon"
+    color = color or "nightfly"
     vim.cmd(' colorscheme ' .. color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMePlease()
