7d46a29a3eb73a4abf6f603ef1e71aefbac2c278
EmmanuelDodoo <custosdodoo@gmail.com>
2023-10-28 18:31:46 -0400

    Added default keymapping to the build runner

--- after/plugin/zz-custom/run.lua
+++ after/plugin/zz-custom/run.lua
@@ -2,9 +2,15 @@
 -- open a terminal and run from there
 
 function Run(cmd)
+    local cmd = cmd or vim.g.builder
     if cmd and cmd ~= "" then
+        vim.cmd("w")
         print(vim.fn.system(cmd))
     else
         print("No or empty command provided")
     end
 end
+
+
+vim.keymap.set("n", "<leader>r", [[:lua Run() <CR>]], {desc = "[r]un the build command for this project. Use vim.g.builder to set the command. Saves the current buffer"})
+
